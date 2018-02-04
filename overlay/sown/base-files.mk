a275a9d14715a58c302a12174b4f6b60f3e4854d
Chris Malton <chrism@cmalton.me.uk>
2018-02-04 18:44:44 +0000

    Add init.d/led overlay.

--- overlay/sown/base-files.mk
+++ overlay/sown/base-files.mk
@@ -6,7 +6,9 @@ define custom_install_commands
         @echo "Installing extra files from $(OVERLAY_DIR)"
         $(INSTALL_DIR) $(1)/etc
         $(INSTALL_DIR) $(1)/etc/rc.local.d
+        $(INSTALL_DIR) $(1)/etc/init.d
         $(INSTALL_DATA) $(OVERLAY_DIR)/files/rc.local $(1)/etc/rc.local
+        $(INSTALL_DATA) $(OVERLAY_DIR)/files/init.d/led $(1)/etc/init.d/led
 endef
 
 # Append custom commands to install recipe,
