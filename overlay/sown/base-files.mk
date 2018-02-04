dc7813dca3359f99cde8dbc2df0d5730f9908b21
Chris Malton <chrism@cmalton.me.uk>
2018-02-04 18:21:32 +0000

    Install rc.local.d

--- overlay/sown/base-files.mk
+++ overlay/sown/base-files.mk
@@ -5,6 +5,7 @@ OVERLAY_DIR:=$(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 define custom_install_commands
         @echo "Installing extra files from $(OVERLAY_DIR)"
         $(INSTALL_DIR) $(1)/etc
+        $(INSTALL_DIR) $(1)/etc/rc.local.d
         $(INSTALL_DATA) $(OVERLAY_DIR)/files/rc.local $(1)/etc/rc.local
 endef
 
